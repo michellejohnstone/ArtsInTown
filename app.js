5db5ee58d200676c9e10ac87e20e78f7aa187cc6
barango <barango@mills.edu>
2017-04-13 03:24:34 +0000

    goodbye app_server, hello angular app_client

--- app.js
+++ app.js
@@ -1,32 +1,20 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+var app = express();
 var bodyParser = require('body-parser');
+var path = require('path');
+var cookieParser = require('cookie-parser');
 require('./app_api/models/db');
 
-var routes = require('./app_server/routes/index');
 var routesApi = require('./app_api/routes/index');
-var users = require('./app_api/routes/users');
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'app_server', 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: 'true' }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
 app.use('/api', routesApi);
-app.use('/users', users);
+app.use(express.static(path.join(__dirname, 'app_client')));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -40,10 +28,14 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  // res.render('error');
+});
+
+
+var server = app.listen(app.get('port'), function() {
+       console.log('I am listening on port ' + server.address().port);
 });
 
 module.exports = app;
